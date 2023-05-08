fe2f557399c6e9956b33b8dd14222385fcd21ca4
Kamil <kamilraczkowski2903@gmail.com>
2023-05-08 18:45:13 +0200

    kmeans initial commit

---
+++ src/kMeans/Main.java
@@ -0,0 +1,17 @@
+package kMeans;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new File(args[0]));
+
+
+    }
+
+
+}
+
